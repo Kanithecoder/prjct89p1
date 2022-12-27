df97e5818138e864348730bcd39bd44751d9cbba
Kanithecoder <92029378+Kanithecoder@users.noreply.github.com>
2022-12-27 20:40:28 +0530

    Add files via upload

---
+++ project 89/mathsquiz.js
@@ -0,0 +1,5 @@
+localStorage.getItem("player1_name", player1_name);
+localStorage.setItem("player2_name", player2_name);
+
+var player1_name= localStorage.getItem('player1_name').value; 
+var player2_name= localStorage.getItem('player2_name').value; 
